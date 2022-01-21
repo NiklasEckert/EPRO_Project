27efbd9db5317925ef7ef39d18e3230dbca44ab7
niklaseckert <niklas.eckert.1807@gmail.com>
2022-01-21 09:48:52 +0100

    Initial commit.

---
+++ src/main/java/de/niklaseckert/epro_project/EproProjectApplication.java
@@ -0,0 +1,13 @@
+package de.niklaseckert.epro_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EproProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EproProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/niklaseckert/epro_project/EproProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.niklaseckert.epro_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EproProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
