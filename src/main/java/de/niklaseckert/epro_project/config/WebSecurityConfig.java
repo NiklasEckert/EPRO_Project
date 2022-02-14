94a72737ae7728ad5855583920f82107da427963
jako264 <jakob.friedsam@gmx.de>
2022-02-14 18:19:56 +0100

    Updated WebSecurityConfig.

--- src/main/java/de/niklaseckert/epro_project/config/WebSecurityConfig.java
+++ src/main/java/de/niklaseckert/epro_project/config/WebSecurityConfig.java
@@ -1,15 +1,42 @@
 package de.niklaseckert.epro_project.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@AllArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable();
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                //.anyRequest().authenticated()
+                .antMatchers("/co/**").hasAuthority("CO_OKR_ADMIN")
+                .antMatchers("/bu/**").hasAuthority("BUO_OKR_ADMIN")
+                .antMatchers("/**").hasAuthority("READ_ONLY")
+                .and().formLogin()
+                .and().httpBasic();
+    }
+
+    @Bean
+    public NoOpPasswordEncoder getPasswordEncode() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 }
