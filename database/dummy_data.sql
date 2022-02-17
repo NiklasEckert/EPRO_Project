35d6d395ff1e8cf60f34f7bb0294eb58e135f996
fabianschulz2 <lisajanine112014>
2022-02-17 15:32:19 +0100

    Removed form-login, added BCryptPasswordEncoder, specified role access.

--- database/dummy_data.sql
+++ database/dummy_data.sql
@@ -1,12 +1,14 @@
-INSERT INTO okr.okr_user VALUES (default, 'ADMIN1', '1NIMDA');
+INSERT INTO okr.okr_user VALUES (default, 'CO_ADMIN', '$2a$10$QAQrdDfrG.f0tk8yVT3ebeYxh02bGunFE0QdO2ZkuZYIvnjLNO1LC'); /*NIMDA_OC*/
+INSERT INTO okr.okr_user VALUES (default, 'BUO_ADMIN', '$2a$10$l5SiZ94PySqqgUY4MxBCKOOrAi6vkpyL.qjps741KjTbp1oTI8scK');/*NIMDA_OUB*/
+INSERT INTO okr.okr_user VALUES (default, 'READ_ONLY', '$2a$10$rbeeL63Dj1p2r.l4olXzMe13FixPGdUZavisSFOXYipe0QdjsVd/m');/*YLNO_DAER*/
 
-INSERT INTO okr.okr_roles VALUES (default, 'READ_ONLY');
-INSERT INTO okr.okr_roles VALUES (default, 'CO_OKR_ADMIN');
-INSERT INTO okr.okr_roles VALUES (default, 'BUO_OKR_ADMIN');
+INSERT INTO okr.okr_roles VALUES (default, 'ROLE_READ_ONLY');
+INSERT INTO okr.okr_roles VALUES (default, 'ROLE_CO_OKR_ADMIN');
+INSERT INTO okr.okr_roles VALUES (default, 'ROLE_BUO_OKR_ADMIN');
 
-INSERT INTO okr.user_roles VALUES (1, 1);
 INSERT INTO okr.user_roles VALUES (1, 2);
-INSERT INTO okr.user_roles VALUES (1, 3);
+INSERT INTO okr.user_roles VALUES (2, 3);
+INSERT INTO okr.user_roles VALUES (3, 1);
 
 INSERT INTO okr.company_objective VALUES (default, 'O1/2022', 'Attraktiv sein für Werkstudenten', 1);
 INSERT INTO okr.company_objective VALUES (default, 'O2/2022', 'Neue Kunden gewinnen', 1);
@@ -24,20 +26,20 @@ INSERT INTO okr.company_objective_key_result VALUES (default, 'KR1', 'Neue Produ
 INSERT INTO okr.company_objective_key_result VALUES (default, 'KR2', 'Produkte weiterentwickeln', 0, 0, 75, 'created', 2, 1);
 INSERT INTO okr.company_objective_key_result VALUES (default, 'KR3', 'Werbeaktionen starten', 0, 0, 60, 'created', 2, 1);
 
-INSERT INTO okr.business_unit_objective VALUES (default, 'O1/2022', 'Team vergrößern', 1, 1);
-INSERT INTO okr.business_unit_objective VALUES (default, 'O2/2022', 'Werbeinteraktion vergrößern', 1, 1);
-INSERT INTO okr.business_unit_objective VALUES (default, 'O3/2022', 'Neue Produkte beworben', 1, 1);
-INSERT INTO okr.business_unit_objective VALUES (default, 'O4/2022', 'Weihnachtsgeschäft verbessern', 1, 1);
+INSERT INTO okr.business_unit_objective VALUES (default, 'O1/2022', 'Team vergrößern', 1, 2);
+INSERT INTO okr.business_unit_objective VALUES (default, 'O2/2022', 'Werbeinteraktion vergrößern', 1, 2);
+INSERT INTO okr.business_unit_objective VALUES (default, 'O3/2022', 'Neue Produkte beworben', 1, 2);
+INSERT INTO okr.business_unit_objective VALUES (default, 'O4/2022', 'Weihnachtsgeschäft verbessern', 1, 2);
 
-INSERT INTO okr.business_unit_objective VALUES (default, 'O1/2022', 'Team vergrößern', 2, 1);
-INSERT INTO okr.business_unit_objective VALUES (default, 'O2/2022', 'Neue Produkte entwickeln', 2, 1);
-INSERT INTO okr.business_unit_objective VALUES (default, 'O3/2022', 'Produkte finalisieren', 2, 1);
-INSERT INTO okr.business_unit_objective VALUES (default, 'O4/2022', 'Neue Techniken ausprobieren', 2, 1);
+INSERT INTO okr.business_unit_objective VALUES (default, 'O1/2022', 'Team vergrößern', 2, 2);
+INSERT INTO okr.business_unit_objective VALUES (default, 'O2/2022', 'Neue Produkte entwickeln', 2, 2);
+INSERT INTO okr.business_unit_objective VALUES (default, 'O3/2022', 'Produkte finalisieren', 2, 2);
+INSERT INTO okr.business_unit_objective VALUES (default, 'O4/2022', 'Neue Techniken ausprobieren', 2, 2);
 
-INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR1', 'Anzeigen Uni Mainz', 0, 10, 60, 'created', 1, 1, 1);
-INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR2', 'Praktika Uni Mainz', 0, 5, 50, 'created', 1, 2, 1);
-INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR3', 'Seminare Uni Mainz', 0, 3, 45, 'created', 1, 3, 1);
+INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR1', 'Anzeigen Uni Mainz', 0, 10, 60, 'created', 1, 1, 2);
+INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR2', 'Praktika Uni Mainz', 0, 5, 50, 'created', 1, 2, 2);
+INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR3', 'Seminare Uni Mainz', 0, 3, 45, 'created', 1, 3, 2);
 
-INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR1', 'Anzeigen TH Bingen', 0, 10, 30, 'created', 5, 1, 1);
-INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR2', 'Praktika TH Bingen', 0, 5, 77, 'created', 5, 2, 1);
-INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR3', 'Seminare TH Bingen', 0, 3, 98, 'created', 5, 3, 1);
+INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR1', 'Anzeigen TH Bingen', 0, 10, 30, 'created', 5, 1, 2);
+INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR2', 'Praktika TH Bingen', 0, 5, 77, 'created', 5, 2, 2);
+INSERT INTO okr.business_unit_objective_key_result VALUES (default, 'KR3', 'Seminare TH Bingen', 0, 3, 98, 'created', 5, 3, 2);
--- src/main/java/de/niklaseckert/epro_project/config/WebSecurityConfig.java
+++ src/main/java/de/niklaseckert/epro_project/config/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package de.niklaseckert.epro_project.config;
 
+import de.niklaseckert.epro_project.service.OkrBasicAuthenticationEntryPoint;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @Configuration
 @AllArgsConstructor
@@ -18,7 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
-
+    private final OkrBasicAuthenticationEntryPoint authenticationEntryPoint;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         super.configure(auth);
@@ -27,16 +31,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                //.anyRequest().authenticated()
-                .antMatchers("/co/**").hasAuthority("CO_OKR_ADMIN")
-                .antMatchers("/bu/**").hasAuthority("BUO_OKR_ADMIN")
-                .antMatchers("/**").hasAuthority("READ_ONLY")
-                .and().formLogin()
-                .and().httpBasic();
+                .antMatchers(HttpMethod.GET, "/co/**").hasAnyRole("CO_OKR_ADMIN", "READ_ONLY")
+                .antMatchers(HttpMethod.POST, "/co/**").hasRole("CO_OKR_ADMIN")
+                .antMatchers(HttpMethod.PUT, "/co/**").hasRole("CO_OKR_ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/co/**").hasRole("CO_OKR_ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/co/**").hasRole("CO_OKR_ADMIN")
+                .antMatchers(HttpMethod.GET, "/bu/**").hasAnyRole("BUO_OKR_ADMIN", "READ_ONLY")
+                .antMatchers(HttpMethod.POST, "/bu/**").hasRole("BUO_OKR_ADMIN")
+                .antMatchers(HttpMethod.PUT, "/bu/**").hasRole("BUO_OKR_ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/bu/**").hasRole("BUO_OKR_ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/bu/**").hasRole("BUO_OKR_ADMIN")
+                //.antMatchers(HttpMethod.GET,"/co/**","/bu/**").hasRole("READ_ONLY")
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .csrf().disable();
     }
 
     @Bean
-    public NoOpPasswordEncoder getPasswordEncode() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    public BCryptPasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
+
 }
