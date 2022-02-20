3a22f4fcbb91f6b6ccf80181d8d2d37d39bf5152
niklaseckert <niklas.eckert.1807@gmail.com>
2022-02-20 16:13:47 +0100

    Implemented DashboardController to serve the dashboard.html.

---
+++ src/main/java/de/niklaseckert/epro_project/controller/DashboardController.java
@@ -0,0 +1,25 @@
+package de.niklaseckert.epro_project.controller;
+
+import de.niklaseckert.epro_project.repos.BusinessUnitRepository;
+import de.niklaseckert.epro_project.repos.CompanyObjectiveRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    private final CompanyObjectiveRepository companyObjectiveRepository;
+    private final BusinessUnitRepository businessUnitRepository;
+
+    @GetMapping
+    public String getDashboard(Model model) {
+        model.addAttribute("company_objectives", companyObjectiveRepository.findAll());
+        model.addAttribute("business_units", businessUnitRepository.findAll());
+        return "dashboard";
+    }
+}
