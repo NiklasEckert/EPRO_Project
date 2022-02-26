6d720a1ed5feff49860bbf78e856fbb3b393dbc0
jako264 <jakob.friedsam@gmx.de>
2022-02-26 15:20:16 +0100

    Implemented ExceptionHandler.

---
+++ src/main/java/de/niklaseckert/epro_project/controller/exceptions/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,27 @@
+package de.niklaseckert.epro_project.controller.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {
+            BusinessUnitNotFoundException.class,
+            BusinessUnitObjectiveNotFoundException.class,
+            BusinessUnitObjectivesKeyResultNotFoundException.class,
+            CompanyObjectiveNotFoundException.class,
+            CompanyObjectiveKeyResultNotFoundException.class,
+            UsernameNotFoundException.class
+    })
+    protected ResponseEntity<Object> handleConflict(RuntimeException exception, WebRequest webRequest) {
+        String bodyOfResponse = exception.getMessage();
+        return handleExceptionInternal(exception, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
+    }
+}
