57b8e61d6e57535649347862921e9089aca7219b
jako264 <jakob.friedsam@gmx.de>
2022-02-27 21:24:44 +0100

    Changed HistoryCompanyObjectiveKeyResultRepository to contain HistoryCompanyObjectiveKeyResults.

--- src/main/java/de/niklaseckert/epro_project/repos/HistoryCompanyObjectiveKeyResultRepository.java
+++ src/main/java/de/niklaseckert/epro_project/repos/HistoryCompanyObjectiveKeyResultRepository.java
@@ -1,9 +1,9 @@
 package de.niklaseckert.epro_project.repos;
 
-import de.niklaseckert.epro_project.model.HistoryBusinessUnitObjectiveKeyResult;
+import de.niklaseckert.epro_project.model.HistoryCompanyObjectiveKeyResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HistoryCompanyObjectiveKeyResultRepository extends JpaRepository<HistoryBusinessUnitObjectiveKeyResult, Long> {
+public interface HistoryCompanyObjectiveKeyResultRepository extends JpaRepository<HistoryCompanyObjectiveKeyResult, Long> {
 }
